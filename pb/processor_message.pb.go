575d1305e9900b74c363aa078f63b5369d764f5e
Saurav Adhikari <sauravads123@gmail.com>
2022-06-28 08:58:00 +0545

    feat : updated proto files and added clang-format support

--- pb/processor_message.pb.go
+++ pb/processor_message.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.21.1
 // source: processor_message.proto
 
-package basic_gRPC_protobuf
+package pb
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
@@ -107,6 +107,77 @@ func (x *CPU) GetMaxGhz() float64 {
 	return 0
 }
 
+type GPU struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Brand  string  `protobuf:"bytes,1,opt,name=brand,proto3" json:"brand,omitempty"`
+	Name   string  `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	MinGhz float64 `protobuf:"fixed64,5,opt,name=min_ghz,json=minGhz,proto3" json:"min_ghz,omitempty"`
+	MaxGhz float64 `protobuf:"fixed64,6,opt,name=max_ghz,json=maxGhz,proto3" json:"max_ghz,omitempty"`
+}
+
+func (x *GPU) Reset() {
+	*x = GPU{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_processor_message_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GPU) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GPU) ProtoMessage() {}
+
+func (x *GPU) ProtoReflect() protoreflect.Message {
+	mi := &file_processor_message_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GPU.ProtoReflect.Descriptor instead.
+func (*GPU) Descriptor() ([]byte, []int) {
+	return file_processor_message_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *GPU) GetBrand() string {
+	if x != nil {
+		return x.Brand
+	}
+	return ""
+}
+
+func (x *GPU) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *GPU) GetMinGhz() float64 {
+	if x != nil {
+		return x.MinGhz
+	}
+	return 0
+}
+
+func (x *GPU) GetMaxGhz() float64 {
+	if x != nil {
+		return x.MaxGhz
+	}
+	return 0
+}
+
 var File_processor_message_proto protoreflect.FileDescriptor
 
 var file_processor_message_proto_rawDesc = []byte{
@@ -122,10 +193,14 @@ var file_processor_message_proto_rawDesc = []byte{
 	0x65, 0x72, 0x54, 0x68, 0x72, 0x65, 0x61, 0x64, 0x73, 0x12, 0x17, 0x0a, 0x07, 0x6d, 0x69, 0x6e,
 	0x5f, 0x67, 0x68, 0x7a, 0x18, 0x05, 0x20, 0x01, 0x28, 0x01, 0x52, 0x06, 0x6d, 0x69, 0x6e, 0x47,
 	0x68, 0x7a, 0x12, 0x17, 0x0a, 0x07, 0x6d, 0x61, 0x78, 0x5f, 0x67, 0x68, 0x7a, 0x18, 0x06, 0x20,
-	0x01, 0x28, 0x01, 0x52, 0x06, 0x6d, 0x61, 0x78, 0x47, 0x68, 0x7a, 0x42, 0x28, 0x5a, 0x26, 0x67,
-	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6e, 0x33, 0x72, 0x64, 0x6b, 0x69,
-	0x64, 0x2f, 0x62, 0x61, 0x73, 0x69, 0x63, 0x2d, 0x67, 0x52, 0x50, 0x43, 0x2d, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x62, 0x75, 0x66, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x01, 0x28, 0x01, 0x52, 0x06, 0x6d, 0x61, 0x78, 0x47, 0x68, 0x7a, 0x22, 0x61, 0x0a, 0x03, 0x47,
+	0x50, 0x55, 0x12, 0x14, 0x0a, 0x05, 0x62, 0x72, 0x61, 0x6e, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x05, 0x62, 0x72, 0x61, 0x6e, 0x64, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x17, 0x0a, 0x07,
+	0x6d, 0x69, 0x6e, 0x5f, 0x67, 0x68, 0x7a, 0x18, 0x05, 0x20, 0x01, 0x28, 0x01, 0x52, 0x06, 0x6d,
+	0x69, 0x6e, 0x47, 0x68, 0x7a, 0x12, 0x17, 0x0a, 0x07, 0x6d, 0x61, 0x78, 0x5f, 0x67, 0x68, 0x7a,
+	0x18, 0x06, 0x20, 0x01, 0x28, 0x01, 0x52, 0x06, 0x6d, 0x61, 0x78, 0x47, 0x68, 0x7a, 0x42, 0x06,
+	0x5a, 0x04, 0x2e, 0x2f, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -140,9 +215,10 @@ func file_processor_message_proto_rawDescGZIP() []byte {
 	return file_processor_message_proto_rawDescData
 }
 
-var file_processor_message_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_processor_message_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_processor_message_proto_goTypes = []interface{}{
 	(*CPU)(nil), // 0: pb.CPU
+	(*GPU)(nil), // 1: pb.GPU
 }
 var file_processor_message_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -170,6 +246,18 @@ func file_processor_message_proto_init() {
 				return nil
 			}
 		}
+		file_processor_message_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GPU); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -177,7 +265,7 @@ func file_processor_message_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_processor_message_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
